e0961d9a53b4630cef68965e59b93b5baefcb640
Augusto94-coder <augustodileo94@gmail.com>
2025-09-01 16:36:30 +0200

    first commit - algoritmo esercizio dadi

---
+++ script.js
@@ -0,0 +1,9 @@
+/* 
+Algoritmo esercizio dadi:
+1 - Definisco array di stringhe, con mail inventate
+2 - Chiedo all'utente di inserire la propria mail tramite prompt
+3 - Scorro la lista con un for, confrontando con ogni elemento della lista
+4 - IF emailUtenti === listainvitati[i]
+    - stampa messaggio di successo
+    ELSE stampa altro messaggio
+*/
