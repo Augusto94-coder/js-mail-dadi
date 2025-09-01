654c0d5e32978f9d60216d5ae5e2c061ecc5a928
Augusto94-coder <augustodileo94@gmail.com>
2025-09-01 16:58:06 +0200

    esercizio mail svolto

--- script.js
+++ script.js
@@ -2,8 +2,37 @@
 Algoritmo esercizio dadi:
 1 - Definisco array di stringhe, con mail inventate
 2 - Chiedo all'utente di inserire la propria mail tramite prompt
-3 - Scorro la lista con un for, confrontando con ogni elemento della lista
+3 - Definisco e inizializzo variabile di controllo a false (caso standard non posso entrare alla festa). Scorro la lista con un for, confrontando con ogni elemento della lista
 4 - IF emailUtenti === listainvitati[i]
     - stampa messaggio di successo
     ELSE stampa altro messaggio
 */
+
+let listaInvitati = [
+  "asdrubale.rossi@libero.com",
+  "anna.pannocchia@libero.com",
+  "ivo.avido@libero.com",
+  "brunoliegi.bastonliegi@libero.com"
+];
+
+
+let emailUtente = prompt("Inserisci la tua email per accedere alla festa:");
+
+
+let trovato = false;
+
+
+for (let i = 0; i < listaInvitati.length; i++) {
+  if (emailUtente === listaInvitati[i]) {
+    trovato = true;
+  }
+}
+
+
+if (trovato) {
+  console.log("Benvenuto/a! Puoi entrare alla festa");
+  alert("Benvenuto/a! Puoi entrare alla festa");
+} else {
+  console.log("Spiaze, la tua email non è nella lista degli invitati");
+  alert("Spiaze, la tua email non è nella lista degli invitati");
+}
